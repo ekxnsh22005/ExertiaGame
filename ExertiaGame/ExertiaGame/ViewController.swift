ce0c86ddea01393c88f1ab40af73b21f28475793
Ekansh Jindal <138109582+ekxnsh22005@users.noreply.github.com>
2025-12-10 15:53:55 +0530

    Swift project setup

---
+++ ExertiaGame/ExertiaGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExertiaGame
+//
+//  Created by Ekansh Jindal on 10/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
